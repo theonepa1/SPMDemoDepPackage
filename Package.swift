9492f884590acf6d2f6eaefa528e4719c6156282
theonepa1 <theone.pa1@gmail.com>
2023-02-03 13:52:59 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
             targets: ["SPMDemoDep"])
     ],
     dependencies: [
-        .package(url: "https://github.com/theonepa1/SPMDemoPackage", revision: "4ffb806ef69a3af5845b751474d810a8b92cfdb2"),
+        .package(url: "https://github.com/socure-inc/socure-sigmadevice-sdk-ios", revision: "b0fc489d02d5b4516ff1566e0a75c37e80088387"),
         .package(url: "https://github.com/datatheorem/TrustKit", from: "2.0.0")
     ],
     targets: [
         .target(
             name: "SPMDemoDep",
             dependencies: [
-                .product(name: "SPMDemoNoTrustKit", package: "SPMDemoPackage"),
+                .product(name: "SigmaDeviceWithoutTrustKit", package: "socure-sigmadevice-sdk-ios"),
                 "TrustKit"
             ] 
         ),
