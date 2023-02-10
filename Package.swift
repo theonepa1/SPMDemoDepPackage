cab3f3fc8aae4fd311c7298796b6c289fca4ad4e
theonepa1 <theone.pa1@gmail.com>
2023-02-09 16:34:47 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["SPMDemoDep"])
     ],
     dependencies: [
-        .package(url: "https://github.com/theonepa1/SPMDemoPackage", revision: "78231f0245046f83dd688ec41c4cfa64c71071eb"),
+        .package(url: "https://github.com/theonepa1/SPMDemoPackage", revision: "28d3f0f11876b637ec23eca62fbce625e8c751c6"),
         .package(url: "https://github.com/datatheorem/TrustKit", from: "2.0.0")
     ],
     targets: [
